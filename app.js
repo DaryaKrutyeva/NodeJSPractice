a6572718a392820a44b0e53b94d297ee35ddf2c5
Darya Krutyeva <85214246+DaryaKrutyeva@users.noreply.github.com>
2022-08-13 19:03:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -20,16 +20,17 @@ const server = http.createServer((req, res) => {
 
 
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parseBody = Buffer.concat(body).toString();
             const message = parseBody.split('=')[1];
             fs.writeFileSync('message.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
 
         })
        
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+       
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
