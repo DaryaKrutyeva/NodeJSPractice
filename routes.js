33fa3cd14e1e75cb2eb422722cbc931daba6fed4
Darya Krutyeva <85214246+DaryaKrutyeva@users.noreply.github.com>
2022-08-13 19:28:38 -0500

    update structure

---
+++ routes.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+
+
+const requestHandler =(req, res) =>{
+    const url = req.url;
+    const method = req.method;
+
+    if (url ==='/') {
+        res.write('<html>');
+        res.write('<head><title> Enter Message </title> </head>');
+        res.write('<body><form action="/message" method="POST"><input type ="text" name="message"><button type = "submit"> SEND </button></form></body>');
+        res.write('</html>');
+        return res.end();
+    
+    }
+    
+    if(url ==='/message' && method ==='POST'){
+        const body =[];
+        req.on('data', (chunk) =>{
+            body.push(chunk);
+    
+    
+        });
+        //event handling architecture where I request NodeJS to perform some action and it 
+        // will offload the request to the operating system which does support multithreading
+        //never blocks code and server
+        return req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+           
+    
+        })
+       
+       
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title> <head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
+};
+
+
+module.exports = requestHandler;
+
