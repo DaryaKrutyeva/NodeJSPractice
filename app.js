af5574f376e82d40a5ef8eb650a10100beafbf82
Darya Krutyeva <85214246+DaryaKrutyeva@users.noreply.github.com>
2022-08-13 19:20:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ server.listen(3000);
  *    then checks pending callbacks, i-o (file, network) related that were deferred
 *    -3rd: poll phase - looks for new io events and executed callback immediately if possible or defer
 *    -4th check: setImmediate callbacks
+    -5th: close - execute all close event callbacks
 * -worker pool: runs on different threads, detached from code
  *      -once worker is done, will trigger callback, which will end up in event loop where it will be executed
  */
